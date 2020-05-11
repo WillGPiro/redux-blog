ed3bb869eadd1217c5521ca60c7d6ba61189628d
Will G Piro <willgpiro@gmail.com>
2020-05-11 15:20:12 -0700

    getPostsByIndex selector

--- src/selectors/postSelectors.js
+++ src/selectors/postSelectors.js
@@ -1,2 +1,4 @@
 export const getPosts = state => state;
 
+export const getPostsByIndex = (state, index) => (state.post[index])
+
--- src/selectors/postSelectors.test.js
+++ src/selectors/postSelectors.test.js
@@ -1,7 +1,8 @@
-import { getPosts } from "./postSelectors";
+import { getPosts, getPostsByIndex } from "./postSelectors";
+
 
 describe('post selectors', () => {
-  it('gets all (list) of dogs', () => {
+  it('gets all (list) of posts', () => {
     const state = [
       {
         title: 'My Lost Ocean - A Haiku',
@@ -18,4 +19,29 @@ describe('post selectors', () => {
       }
     ]);
   });
+
+  it('gets a post by idex', () => {
+    const state = {
+      post: [
+      {
+        title: 'My Lost Ocean - A Haiku',
+        body: 'Spring, the days grow long, your ebb and flow call my name, alas I am home.'
+      },
+      {
+        title: 'My Lost Mountain - A Haiku',
+        body: 'Winter, the days grow short, your cold and snow call my name, alas I write code.'
+      },
+      {
+        title: 'My Lost Friends - A Haiku',
+        body: 'Summer, the days are long, your sun and warmth call my name, Im still home.'
+      }
+    ]};
+
+    const post = getPostsByIndex(state, 0)
+
+    expect(post).toEqual({
+      title: 'My Lost Ocean - A Haiku',
+      body: 'Spring, the days grow long, your ebb and flow call my name, alas I am home.'
+    })
+  })
 });
